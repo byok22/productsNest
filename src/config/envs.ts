80a254cafa456764d7e4fc5e8204a8a1e1e81807
Kevin Torruco <Kevin_Torruco@jabil.com>
2024-07-15 11:09:03 -0600

    [ADD] ENV errores

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,8 +1,31 @@
 import 'dotenv/config';
+import *  as joi from 'joi';
+
+interface EnvVars{
+    PORT: number
+}
 
 
 // TODO: validar mediante un esquema
+const envsSchema = joi.object({
+    PORT: joi.number().required()
+
+})
+.unknown(true);
+
+const {error, value} = envsSchema.validate ( process.env );
+
+if( error ){
+    throw new Error(`Config Validation error ${ error.message } `);
+}
+
+
+console.log({error});
+
+const envVars: EnvVars = value;
+
+console.log ({ envVars })
 
 export const envs = {
-    port: process.env.PORT,
+    port: envVars.PORT,
 }
